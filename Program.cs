14502a8c3706cf5f7cd257bc5643640fd8bde6ae
Prodaku <b.isaacpro@gmail.com>
2018-12-18 08:55:38 +0100

    Fini

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Veuillez renseignez votre NOM.");
+            string nom = Console.ReadLine();
+
+            Console.WriteLine("Veuillez renseignez votre PRENOM.");
+            string prenom = Console.ReadLine();
+
+            Console.WriteLine("Bonjour Monsieur " + nom + " " + prenom + ", nous sommes le " + DateTime.Now.ToShortDateString() + " comment allez vous ?");
+
+        }
+    }
+}
